da00e020587cf99a82753d577fd625b2f6249317
AnesJusupovic <jusanes04@gmail.com>
2022-07-30 21:03:19 +0200

    Turtle

---
+++ 17and18/main3.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+
+if __name__ == "__main__":
+    timmy_the_turtle = Turtle()
+    timmy_the_turtle.shape("turtle")
+    timmy_the_turtle.color("red")
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    screen = Screen()
+    screen.exitonclick()
