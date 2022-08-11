d5f0a3a017ccbb3ebced7af9b156140c0345c6ef
AnesJusupovic <jusanes04@gmail.com>
2022-08-11 11:51:02 +0200

    Working with pandas

--- 23and24/mail/main.py
+++ 23and24/mail/main.py
@@ -6,3 +6,15 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+if __name__ == "__main__":
+    with open("Input/Names/invited_names.txt") as file:
+        content = file.read()
+        names = content.split("\n")
+        letter = ""
+        with open("Input/Letters/starting_letter.txt") as fileOne:
+            letter = fileOne.read()
+        for name in names:
+            with open(f"Output/ReadyToSend/invited_{name}.txt", mode="w") as fileSecond:
+                fileSecond.write(letter.replace("[name]", name))
+
